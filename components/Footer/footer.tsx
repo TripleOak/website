ca8fad25aa19c7abdfeafdf44742a551d64cd48d
João Carvalho <joao.carvalho@tripleoak.pt>
2022-12-31 14:02:33 +0000

    fix footer

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 function Footer() {
   return (
     <footer className="mt-16">
-      <div className="pt-2 pb-12 sm:w-11/12 md:w-3/4 mx-auto border-t border-gray-300">
+      <div className="pt-2 pb-12 w-full sm:w-11/12 md:w-3/4 mx-auto border-t border-gray-300">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <div className="flex flex-col mt-4 sm:flex-row sm:space-x-8 sm:mt-0 font-sans text-xs font-light uppercase">
+          <div className="flex flex-row space-x-8 mt-4 ml-8 sm:ml-0 sm:mt-0 font-sans text-xs font-light uppercase">
             <Link href="/terms-of-use">
               <span className="text-gray-400 hover:text-gray-900 cursor-pointer">Terms of use</span>
             </Link>
